dba09579212f25ac5389b868501ed9ae08f82267
Guochen Sun <guochen_s@hotmail.com>
2016-01-29 20:26:00 +0800

    1.29

--- QuickSort/QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort/QuickSort.cpp
@@ -1,6 +1,6 @@
 // QuickSort.cpp : 定义控制台应用程序的入口点。
-////ran = (max - min + 1)*rand() / (RAND_MAX + 1.0) + min;   //产生[min，max]区间的随机数
-
+//ran = (max - min + 1)*rand() / (RAND_MAX + 1.0) + min;   //产生[min，max]区间的随机数
+//ran=rand()%(max-min+1)+min;
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +13,11 @@ public:
 		// write code here
 		int max=n-1,min=0,ran=0;
 		int *temp=new int[n];
-		ran = (max - min + 1)*rand() / (RAND_MAX + 1.0) + min;
+		if (temp == NULL)
+		{
+			return A;
+		}
+		ran = rand() % (max - min + 1) + min;
 		sort(A, max, ran, min,temp);
 		return A;
 	}
@@ -25,26 +29,26 @@ public:
 		tem =quick(a, max, ran, min,temp);
 		if(tem - min > 1)
 		{
-			ra = (ran - 1 - min + 1)*rand() / (RAND_MAX + 1.0) + min;
-			sort(a, ran - 1, ra, min,temp);//左
+			ra = rand()% (tem - 1 - min + 1) + min;
+			sort(a, tem - 1, ra, min,temp);//左
 		}
 		if (max - tem  >1)
 		{
-			ra = (max - ran + 1 + 1)*rand() / (RAND_MAX + 1.0) + min;
-			sort(a, max, ra, ran + 1, temp);//右
+			ra = rand()%(max - tem) +tem + 1;
+			sort(a, max, ra, tem + 1, temp);//右
 		}
 	}
 
 	//划分
 	int quick(int *a, int max,int ran , int min,int *temp)
 	{
-		int i=0, j = 0,num=0;
-		for (i = 0; i <= max; i++)
+		int i=0,num=min;
+		for (i = min; i <= max; i++)
 		{
 			temp[i] = a[i];
 		}
 		swap(temp +max, temp +ran);
-		for (i = 0; i < max;i++)
+		for (i =min; i < max;i++)
 		{
 			if (temp[i] < temp[max])
 			{
@@ -53,7 +57,7 @@ public:
 			}
 		}
 		swap(temp +num, temp +max);
-		for (i = 0; i <= max; i++)
+		for (i = min; i <= max; i++)
 		{
 			a[i] =temp[i] ;
 		}
@@ -75,13 +79,14 @@ int main()
 	int len;
 	//scanf_s("%d", &len);	
 	len = 13;
-	//int max = 6, min = 0, ran = 0;
-	//for (int i = 0; i < 10;i++)
-	//{
-	//	ran = (max - min + 1)*rand() / (RAND_MAX + 1.0) + min;
-	//	printf(" %d ", ran);
-	//}
-	int p[13] = { 54, 35, 48, 36, 27, 12, 44, 44, 8, 14, 26, 17, 28 };
+	/*int max = 6, min = 0, ran = 0;
+	for (int i = 0; i < 10;i++)
+	{
+		//ran = (max - min + 1)*rand() / (RAND_MAX + 1.0) + min;
+		ran = rand();
+		printf(" %d ", ran);
+	}*/
+	int p[13] = { 54,35,48,36,27,12,44,44,8,14,26,17,28 };
 	QuickSort quick;
 	quick.quickSort(p, len);
 	for (int i = 0; i < len; i++)
